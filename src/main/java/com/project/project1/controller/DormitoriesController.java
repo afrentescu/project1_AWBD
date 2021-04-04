b3d38550ea9d5f3241fd5b215b53d44705ea248b
afrentescu <adelafrentescu@gmail.com>
2021-04-04 15:11:26 +0300

    crud dormitory

--- src/main/java/com/project/project1/controller/DormitoriesController.java
+++ src/main/java/com/project/project1/controller/DormitoriesController.java
@@ -1,12 +1,14 @@
 package com.project.project1.controller;
 
 import com.project.project1.model.Course;
+import com.project.project1.model.Department;
 import com.project.project1.model.Dormitory;
 import com.project.project1.service.DormitoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -18,7 +20,27 @@ public class DormitoriesController {
 
 
     @RequestMapping("/dormitories/list")
-    public String dormitoriesList(Model model){
+    public ModelAndView professorsList(){
+        ModelAndView modelAndView = new ModelAndView("dormitories");
         List<Dormitory> dormitories = dormitoryService.findAll();
-        model.addAttribute("dormitories",dormitories);return "dormitories";}
+        modelAndView.addObject("dormitories",dormitories);
+        return modelAndView;
+    }
+
+    @GetMapping("/dormitory/info/{id}")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("dormitory", dormitoryService.findById(Integer.parseInt(id)));
+        return "dormitoryInfo";}
+
+    @RequestMapping("/dormitory/new")
+    public String newCourse(Model model) {
+        model.addAttribute("dormitory", new Dormitory());
+        return "dormitoryAdd";}
+
+    @PostMapping("/dormitory")
+    public String saveOrUpdate(@ModelAttribute Dormitory dormitory)
+    {   dormitoryService.addDormitory(dormitory);
+        return "redirect:/dormitories/list";
+    }
+
 }
